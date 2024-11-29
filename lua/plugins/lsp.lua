925007959c09be6135d5fca04c21a73e2fd700a6
dominika <dominik.albrecht@intersim.ch>
2024-11-29 09:38:24 +0100

    Set lspconfig events, remove dup dep

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -15,6 +15,7 @@ return {
     {
         -- Main LSP Configuration
         'neovim/nvim-lspconfig',
+        events = { "BufReadPost", "BufNewFile", "BufWritePre" },
         dependencies = {
             -- Automatically install LSPs and related tools to stdpath for Neovim
             { 'williamboman/mason.nvim', config = true }, -- NOTE: Must be loaded before dependants
@@ -24,9 +25,6 @@ return {
             -- Useful status updates for LSP.
             -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
             { 'j-hui/fidget.nvim',       opts = {} },
-
-            -- Allows extra capabilities provided by nvim-cmp
-            'hrsh7th/cmp-nvim-lsp',
         },
         config = function()
             vim.api.nvim_create_autocmd('LspAttach', {
